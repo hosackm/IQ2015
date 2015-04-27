23925ea655f0b0c59805050fc76e9edc2b4b6b05
hosackm <hosack.matt@gmail.com>
2015-04-27 13:52:40 -0700

    Added socket.io package and put in server.  Added page to view realtime graph with socket.io stub functions.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
 var nunjucks = require('nunjucks');
-
-var app = express();
 var port = process.argv[2] || 8080;
 
+// Configurations
 nunjucks.configure('views', {autoescape: true, express: app});
-
 app.use(express.static('public'));
-app.use('/', function(req, res){
+
+// Routes
+app.get('/', function(req, res){
   res.render('index.html', {});
+});
+app.get('/realtime', function(req, res){
+  res.render('realtime.html', {});
+});
+
+// Socket.io Logic
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
 
 console.log("Express server running on port: ", port);
-app.listen(port);
+server.listen(port);
