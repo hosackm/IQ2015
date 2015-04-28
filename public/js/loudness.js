86659a3e4ae84da08ec09cc4d359cb2799f7733b
hosackm <hosack.matt@gmail.com>
2015-04-27 20:11:57 -0700

    Consolidated plotting scripts into external js file

---
+++ public/js/loudness.js
@@ -0,0 +1,80 @@
+//Loudness Plot
+  // Global variables. yuck!
+var lsamps = [0, 1, 2, 3];
+var lcur = 0;
+var fftsamps = [];
+//  var socket = io.connect('http://localhost:8080');
+var socket = io.connect('http://mshosa-mba:8080');
+var lseries = new TimeSeries();
+
+var loudchart = new SmoothieChart();
+loudchart.addTimeSeries(lseries, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+loudchart.streamTo(document.getElementById("smoothie-chart"), 500);
+
+// Recieve Loudness data
+socket.on('loudness', function(data){
+  if(!data)
+    return;
+
+  // parse/update global values
+  // Ex. data = {value: 24};
+  data = JSON.parse(data);
+  lcur = data.value;
+  lsamps.push(lcur);
+  lseries.append(new Date().getTime(), lcur);
+  // $("#louddisplay").text(lcur);
+});
+
+// Recieve FFT Data
+socket.on('fft', function(data){
+  if(!data)
+    return;
+
+  // Ex. data = {length: 1024, samples: [0.0, 1.3, ... , 45.0]};
+  data = JSON.parse(data);
+  var l = data.nsamp;
+  fftsamps = data.samples;
+  
+  // make list of dataPoints
+  var points = fftsamps.reduce(function(prev, cur, i, arr){
+    if (i >= arr.length - 1)
+      return prev;
+    prev.data.push({x: i, y: cur});
+    return prev;
+  }, {data: []});
+
+  var chart = new CanvasJS.Chart("fft-chart",
+  {
+    title: {text: "FFT Plot"},
+    width: 1310,
+    height: 500,
+    animationEnabled: true,
+    axisX:{title: "Frequency (Hz)", interval: 3},
+    axisY:{title: "Magnitude (dB)"},
+    legend: {},
+    data: [{
+      name: 'fft1',
+      type: 'area',
+      color: 'rgba(0, 255, 0, 1)',
+      markerSize: 0,
+      dataPoints: points.data
+    }]
+  });
+  //chart.options.data[0].dataPoints = points.data;
+  chart.render();
+  $(".canvasjs-chart-credit").remove();
+});
+
+// Button Clicking Shhhhtuff
+$("#switchview").click(function(){
+  if($("#switchview").text() === "View FFT"){
+    $("#loudplot").addClass("hidden");
+    $("#fftplot").removeClass("hidden");
+    $("#switchview").text("View Loudness");
+  }
+  else{
+    $("#fftplot").addClass("hidden");
+    $("#loudplot").removeClass("hidden");
+    $("#switchview").text("View FFT");
+  }
+});
